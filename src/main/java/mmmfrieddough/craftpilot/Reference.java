f3bcc0387750fd1691ffd8f5f07df7ec4464ba33
Patrizio Spagnardi III <mmmfrieddough@gmail.com>
2025-01-02 19:05:46 -0500

    Add reference

---
+++ src/main/java/mmmfrieddough/craftpilot/Reference.java
@@ -0,0 +1,13 @@
+package mmmfrieddough.craftpilot;
+
+import net.minecraft.MinecraftVersion;
+import fi.dy.masa.malilib.util.StringUtils;
+
+public class Reference {
+    public static final String MOD_ID = "craftpilot";
+    public static final String MOD_NAME = "Craftpilot";
+    public static final String MOD_VERSION = StringUtils.getModVersionString(MOD_ID);
+    public static final String MC_VERSION = MinecraftVersion.CURRENT.getName();
+    public static final String MOD_TYPE = "fabric";
+    public static final String MOD_STRING = MOD_ID + "-" + MOD_TYPE + "-" + MC_VERSION + "-" + MOD_VERSION;
+}
