2bd69102cb0178c978b4f5a7e35ef62cd650da40
Patrizio Spagnardi III <mmmfrieddough@gmail.com>
2025-01-02 19:06:40 -0500

    Add additional options

--- src/main/java/mmmfrieddough/craftpilot/CraftPilot.java
+++ src/main/java/mmmfrieddough/craftpilot/CraftPilot.java
@@ -5,23 +5,30 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 import com.google.common.base.Splitter;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.sql.Ref;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -30,14 +37,36 @@ import java.io.InputStreamReader;
 import net.minecraft.registry.Registries;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Property;
+import net.minecraft.text.Text;
 
-import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import fi.dy.masa.litematica.InitHandler;
+import fi.dy.masa.litematica.data.DataManager;
+import fi.dy.masa.litematica.data.SchematicHolder;
+import fi.dy.masa.litematica.schematic.LitematicaSchematic;
+import fi.dy.masa.litematica.schematic.LitematicaSchematic.SchematicSaveInfo;
+import fi.dy.masa.litematica.schematic.container.LitematicaBlockStateContainer;
+import fi.dy.masa.litematica.schematic.placement.SchematicPlacement;
+import fi.dy.masa.litematica.schematic.placement.SchematicPlacementManager;
+import fi.dy.masa.litematica.selection.AreaSelection;
+import fi.dy.masa.litematica.selection.Box;
+import fi.dy.masa.malilib.event.InitializationHandler;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.ConfigHolder;
+import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import mmmfrieddough.craftpilot.config.ModConfig;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+
+import java.lang.reflect.Field;
+
 public class CraftPilot implements ModInitializer {
-	public static final String MOD_ID = "craftpilot";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	private static ModConfig config;
+	public static final Logger LOGGER = LoggerFactory.getLogger(Reference.MOD_ID);
 	private static final Function<Map.Entry<Property<?>, Comparable<?>>, String> PROPERTY_MAP_PRINTER = new Function<Map.Entry<Property<?>, Comparable<?>>, String>() {
 		@Override
 		public String apply(Map.Entry<Property<?>, Comparable<?>> entry) {
@@ -47,16 +76,14 @@ public class CraftPilot implements ModInitializer {
 		}
 	};
 
-	private String[][][] getBlocksMatrix(PlayerEntity player) {
-		World world = player.getEntityWorld();
-		BlockPos playerPos = player.getBlockPos();
-		int radius = 4; // Since we want an 8x8x8 cube, the radius is half of 8
-		String[][][] blocksMatrix = new String[8][8][8];
+	private String[][][] getBlocksMatrix(World world, BlockPos origin) {
+		int radius = 5; // Since we want an 11x11x11 cube, the radius is 5
+		String[][][] blocksMatrix = new String[11][11][11];
 
-		for (int x = -radius; x < radius; x++) {
-			for (int y = -radius; y < radius; y++) {
-				for (int z = -radius; z < radius; z++) {
-					BlockPos blockPos = playerPos.add(x, y, z);
+		for (int x = -radius; x < radius + 1; x++) {
+			for (int y = -radius; y < radius + 1; y++) {
+				for (int z = -radius; z < radius + 1; z++) {
+					BlockPos blockPos = origin.add(x, y, z);
 					BlockState blockState = world.getBlockState(blockPos);
 					StringBuilder stringBuilder = new StringBuilder();
 					stringBuilder.append(Registries.BLOCK.getId(blockState.getBlock()));
@@ -67,7 +94,7 @@ public class CraftPilot implements ModInitializer {
 						stringBuilder.append(']');
 					}
 					String blockId = stringBuilder.toString();
-					blocksMatrix[x + radius][y + radius][z + radius] = blockId;
+					blocksMatrix[z + radius][y + radius][x + radius] = blockId;
 				}
 			}
 		}
@@ -84,7 +111,6 @@ public class CraftPilot implements ModInitializer {
 		return state.with(prop, (T) value);
 	}
 
-	@Nullable
 	public static <T extends Comparable<T>> T getPropertyValueByName(Property<T> prop, String valStr) {
 		return prop.parse(valStr).orElse(null);
 	}
@@ -134,25 +160,40 @@ public class CraftPilot implements ModInitializer {
 		return Optional.empty();
 	}
 
-	private void setBlockAtRelativePosition(MinecraftClient client, BlockPos origin, List<Integer> relativePosition,
-			String blockId) {
+	public void modifyPrivateField(Object instance, String fieldName, Object newValue) {
+		try {
+			// Get the Field object for the specified field
+			Field field = instance.getClass().getDeclaredField(fieldName);
 
-		int radius = 4; // The radius used in getBlocksMatrix
-		BlockPos targetPos = origin.add(relativePosition.get(0) - radius, relativePosition.get(1) - radius,
-				relativePosition.get(2) - radius);
-		Optional<BlockState> stateOptional = getBlockStateFromString(blockId);
+			// Make the field accessible
+			field.setAccessible(true);
 
-		stateOptional.ifPresent(blockState -> {
-			client.world.setBlockState(targetPos, blockState);
-		});
+			// Set the field's value
+			field.set(instance, newValue);
+		} catch (NoSuchFieldException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
 	}
 
-	private void sendHttpRequest(MinecraftClient client, World world, BlockPos playerPos, String[][][] matrix,
-			double temperature) {
+	private CompletableFuture<HttpResponse<InputStream>> currentRequestFuture = null;
+
+	private void sendHttpRequest(String[][][] matrix, double temperature, int startRadius, int maxIterations,
+			int maxBlocks, float airProbabilityIterationScaling) {
+		// Cancel the ongoing request if it exists
+		if (currentRequestFuture != null && !currentRequestFuture.isDone()) {
+			currentRequestFuture.cancel(true);
+		}
+
 		Gson gson = new Gson();
 		Request request = new Request();
-		request.setStructure(matrix);
+		request.setPlatform("java");
+		request.setVersion_number(3700);
 		request.setTemperature(temperature);
+		request.setStart_radius(startRadius);
+		request.setMax_iterations(maxIterations);
+		request.setMax_blocks(maxBlocks);
+		request.setAir_probability_iteration_scaling(airProbabilityIterationScaling);
+		request.setStructure(matrix);
 		String jsonPayload = gson.toJson(request);
 		HttpClient httpClient = HttpClient.newHttpClient();
 		HttpRequest httpRequest = HttpRequest.newBuilder()
@@ -161,46 +202,146 @@ public class CraftPilot implements ModInitializer {
 				.POST(HttpRequest.BodyPublishers.ofString(jsonPayload))
 				.build();
 
-		httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofInputStream())
-				.thenAccept(inputStream -> {
-					LOGGER.info("Response status code: " + inputStream.statusCode());
-					try (BufferedReader reader = new BufferedReader(
-							new InputStreamReader(inputStream.body(), StandardCharsets.UTF_8))) {
-						String line;
-						while ((line = reader.readLine()) != null) {
-							LOGGER.info(line);
-							ResponseItem responseItem = gson.fromJson(line, ResponseItem.class);
-							String value = responseItem.getValue();
-							List<Integer> position = responseItem.getPosition();
-							setBlockAtRelativePosition(client, playerPos, position, value);
-							LOGGER.info("Value: " + value + ", Position: " + position);
+		// Store the CompletableFuture of the new request
+		currentRequestFuture = httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofInputStream());
 
-						}
-					} catch (Exception e) {
-						LOGGER.error("Error handling the streaming response", e);
-					}
-				})
+		currentRequestFuture.thenAccept(inputStream -> {
+			LOGGER.info("Response status code: " + inputStream.statusCode());
+			if (inputStream.statusCode() != 200) {
+				LOGGER.error("Error sending HTTP request");
+				return;
+			}
+			try (BufferedReader reader = new BufferedReader(
+					new InputStreamReader(inputStream.body(), StandardCharsets.UTF_8))) {
+				String line;
+				while ((line = reader.readLine()) != null) {
+					LOGGER.info(line);
+					ResponseItem responseItem = gson.fromJson(line, ResponseItem.class);
+					responseQueue.offer(responseItem);
+				}
+			} catch (Exception e) {
+				LOGGER.error("Error handling the streaming response", e);
+			}
+		})
 				.exceptionally(e -> {
 					LOGGER.error("Error sending HTTP request", e);
 					return null;
 				});
 	}
 
+	private ConcurrentLinkedQueue<ResponseItem> responseQueue = new ConcurrentLinkedQueue<>();
+	private LitematicaSchematic schematic;
+	private BlockPos pos1;
+	private LitematicaBlockStateContainer container;
+	private BlockPos lastInteractedBlockPos;
+	private boolean blockPlacementPending = false;
+	private SchematicPlacement placement;
+
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		AutoConfig.register(ModConfig.class, Toml4jConfigSerializer::new);
+		config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
 
 		KeyBindings.register(); // Register the key bindings
 
-		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			while (KeyBindings.getExampleKeyBinding().wasPressed()) {
-				LOGGER.info("Key was pressed!");
+		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+			if (config.client.enable == false) {
+				return ActionResult.PASS;
+			}
+			if (world.isClient) {
+				lastInteractedBlockPos = hitResult.getBlockPos();
+				blockPlacementPending = true;
+			}
+			return ActionResult.PASS;
+		});
 
-				PlayerEntity player = client.player;
-				World world = player.getEntityWorld();
-				BlockPos playerPos = player.getBlockPos();
-				String[][][] matrix = getBlocksMatrix(player);
-				sendHttpRequest(client, world, playerPos, matrix, 0.7);
+		ClientTickEvents.END_WORLD_TICK.register(world -> {
+			if (!blockPlacementPending || lastInteractedBlockPos == null) {
+				return;
+			}
+			BlockState state = world.getBlockState(lastInteractedBlockPos);
+			// Check if the block at the position is not air (indicating a block has been
+			// placed)
+			if (state.isAir()) {
+				return;
+			}
+			// Check if the block at the position matches what is in the schematic
+			if (container != null && placement != null) {
+				BlockPos placementOrigin = placement.getOrigin();
+				if (!(lastInteractedBlockPos.getX() < placementOrigin.getX()
+						|| lastInteractedBlockPos.getX() >= placementOrigin.getX() + 11
+						|| lastInteractedBlockPos.getY() < placementOrigin.getY()
+						|| lastInteractedBlockPos.getY() >= placementOrigin.getY() + 11
+						|| lastInteractedBlockPos.getZ() < placementOrigin.getZ()
+						|| lastInteractedBlockPos.getZ() >= placementOrigin.getZ() + 11)) {
+					BlockPos relativePos = lastInteractedBlockPos.subtract(placementOrigin);
+					try {
+						BlockState schematicState = container.get(relativePos.getX(), relativePos.getY(),
+								relativePos.getZ());
+						if (schematicState != null && schematicState == state) {
+							blockPlacementPending = false;
+							return;
+						}
+					} catch (ArrayIndexOutOfBoundsException e) {
+						LOGGER.info("Block position out of bounds");
+					}
+				}
+			}
+
+			AreaSelection selection = new AreaSelection();
+			pos1 = lastInteractedBlockPos.add(-5, -5, -5);
+			BlockPos pos2 = lastInteractedBlockPos.add(5, 5, 5);
+			Box box = new Box();
+			box.setPos1(pos1);
+			box.setPos2(pos2);
+			selection.addSubRegionBox(box, true);
+			SchematicSaveInfo info = new LitematicaSchematic.SchematicSaveInfo(false, false);
+			schematic = LitematicaSchematic.createFromWorld(world, selection, info, "CraftPilot", str -> {
+			});
+			Field field;
+			try {
+				field = schematic.getClass().getDeclaredField("blockContainers");
+			} catch (NoSuchFieldException | SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return;
+			}
+			field.setAccessible(true);
+			Map<String, LitematicaBlockStateContainer> blockContainers;
+			try {
+				blockContainers = ((Map<String, LitematicaBlockStateContainer>) field.get(schematic));
+			} catch (IllegalArgumentException | IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return;
+			}
+			container = blockContainers.values().iterator().next();
+			SchematicHolder.getInstance().addSchematic(schematic, true);
+
+			String[][][] matrix = getBlocksMatrix(world, lastInteractedBlockPos);
+			sendHttpRequest(matrix, config.model.temperature, config.model.startRadius, config.model.maxIterations,
+					config.model.maxBlocks, config.model.airProbabilityIterationScaling);
+			blockPlacementPending = false;
+		});
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			ResponseItem item;
+			while ((item = responseQueue.poll()) != null) {
+				String value = item.getValue();
+				List<Integer> position = item.getPosition();
+				LOGGER.info("Value: " + value + ", Position: " + position);
+				Optional<BlockState> stateOptional = getBlockStateFromString(value);
+				stateOptional.ifPresent(blockState -> {
+					container.set(position.get(2), position.get(1), position.get(0), blockState);
+				});
+
+				SchematicPlacementManager manager = DataManager.getSchematicPlacementManager();
+				if (manager.getSelectedSchematicPlacement() != null) {
+					manager.removeSchematicPlacement(manager.getSelectedSchematicPlacement());
+				}
+				placement = SchematicPlacement.createFor(schematic, pos1, "test", true, true);
+				manager.addSchematicPlacement(placement, true);
+				manager.setSelectedSchematicPlacement(placement);
 			}
 		});
 	}
--- src/main/java/mmmfrieddough/craftpilot/Request.java
+++ src/main/java/mmmfrieddough/craftpilot/Request.java
@@ -1,16 +1,29 @@
 package mmmfrieddough.craftpilot;
 
 public class Request {
-    private String[][][] structure;
+    private String platform;
+    private int version_number;
     private double temperature;
+    private int start_radius;
+    private int max_iterations;
+    private int max_blocks;
+    private float air_probability_iteration_scaling;
+    private String[][][] structure;
 
-    // Getters and setters for 'structure' and 'temperature'
-    public String[][][] getStructure() {
-        return structure;
+    public String getPlatform() {
+        return platform;
     }
 
-    public void setStructure(String[][][] structure) {
-        this.structure = structure;
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public int getVersion_number() {
+        return version_number;
+    }
+
+    public void setVersion_number(int versionNumber) {
+        this.version_number = versionNumber;
     }
 
     public double getTemperature() {
@@ -19,5 +32,45 @@ public class Request {
 
     public void setTemperature(double temperature) {
         this.temperature = temperature;
+    }
+
+    public int getStart_radius() {
+        return start_radius;
+    }
+
+    public void setStart_radius(int startRadius) {
+        this.start_radius = startRadius;
+    }
+
+    public int getMax_iterations() {
+        return max_iterations;
+    }
+
+    public void setMax_iterations(int maxIterations) {
+        this.max_iterations = maxIterations;
+    }
+
+    public int getMax_blocks() {
+        return max_blocks;
+    }
+
+    public void setMax_blocks(int maxBlocks) {
+        this.max_blocks = maxBlocks;
+    }
+
+    public float getAir_probability_iteration_scaling() {
+        return air_probability_iteration_scaling;
+    }
+
+    public void setAir_probability_iteration_scaling(float airProbabilityIterationScaling) {
+        this.air_probability_iteration_scaling = airProbabilityIterationScaling;
+    }
+
+    public String[][][] getStructure() {
+        return structure;
+    }
+
+    public void setStructure(String[][][] structure) {
+        this.structure = structure;
     }
 }
