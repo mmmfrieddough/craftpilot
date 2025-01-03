bd79183d54d74182e25aa4d5bc994004da0bd6c2
Patrizio Spagnardi III <mmmfrieddough@gmail.com>
2025-01-02 19:06:33 -0500

    Add config

---
+++ src/main/java/mmmfrieddough/craftpilot/config/ModConfig.java
@@ -0,0 +1,36 @@
+package mmmfrieddough.craftpilot.config;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry.Category;
+import me.shedaniel.autoconfig.annotation.ConfigEntry.Gui.TransitiveObject;
+import me.shedaniel.autoconfig.annotation.ConfigEntry.Gui.Tooltip;
+
+@Config(name = mmmfrieddough.craftpilot.Reference.MOD_ID)
+public class ModConfig implements ConfigData {
+    @Category("Client")
+    @TransitiveObject
+    public Client client = new Client();
+
+    public static class Client {
+        @Tooltip
+        public boolean enable = true;
+    }
+
+    @Category("Model")
+    @TransitiveObject
+    public Model model = new Model();
+
+    public static class Model {
+        @Tooltip
+        public float temperature = 0.7f;
+        @Tooltip
+        public int startRadius = 1;
+        @Tooltip
+        public int maxIterations = 5;
+        @Tooltip
+        public int maxBlocks = 20;
+        @Tooltip
+        public float airProbabilityIterationScaling = 0.0f;
+    }
+}
---
+++ src/main/java/mmmfrieddough/craftpilot/config/ModMenuIntegration.java
@@ -0,0 +1,16 @@
+package mmmfrieddough.craftpilot.config;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfig;
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.EnvType;
+
+@Environment(EnvType.CLIENT)
+public class ModMenuIntegration implements ModMenuApi {
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
+    }
+}
