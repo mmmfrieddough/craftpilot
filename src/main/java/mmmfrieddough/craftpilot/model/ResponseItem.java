bfe8b41f45ab7a03fc3b498f740206968b6bbb46
Patrizio Spagnardi III <mmmfrieddough@gmail.com>
2025-03-12 12:47:47 -0400

    Add alternatives option Change request format Fix generation stopping bug

--- src/main/java/mmmfrieddough/craftpilot/model/ResponseItem.java
+++ src/main/java/mmmfrieddough/craftpilot/model/ResponseItem.java
@@ -1,16 +1,22 @@
 package mmmfrieddough.craftpilot.model;
 
 public class ResponseItem {
+    private int alternative_num;
     private String block_state;
     private int x;
     private int y;
     private int z;
 
-    public ResponseItem(String blockState, int x, int y, int z) {
+    public ResponseItem(int alternativeNum, String blockState, int x, int y, int z) {
+        this.alternative_num = alternativeNum;
         this.block_state = blockState;
         this.x = x;
         this.y = y;
         this.z = z;
+    }
+
+    public int getAlternativeNum() {
+        return alternative_num;
     }
 
     public String getBlockState() {
